a70bb9fafa0f1bfa5313ffeb6f7d5e2e28d02cbd
zhangq327 <81618067+zhangq327@users.noreply.github.com>
2022-10-14 15:02:10 +0800

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -1,12 +1,12 @@
 
 
-CUDA_VISIBLE_DEVICES=8 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=2 main_linprobe.py \
-    --accum_iter 1 \
+CUDA_VISIBLE_DEVICES=8 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=8 main_linprobe.py \
+    --accum_iter 4 \
     --batch_size 256 \
     --model vit_base_patch16 --cls_token\
-    --finetune output_dir/baseline-200-base/checkpoint-180.pth \
+    --finetune checkpoint_dir\
     --epochs 90 \
     --blr 0.1  \
     --weight_decay 0.0 \
     --log_dir temp_dir\
-    --dist_eval --data_path data/imagenet100
+    --dist_eval --data_path data
