bbd26c1eb919419f4a80469f770b125bfc6ab082
zhangq327 <81618067+zhangq327@users.noreply.github.com>
2022-10-15 21:18:47 +0800

    Update engine_pretrain.py

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -61,7 +61,7 @@ def train_one_epoch(model: torch.nn.Module,
             
             loss_ce = torch.nn.functional.cross_entropy(outputs, targets)
 
-        loss = loss_mae + args.lambda1 * loss_reg + loss_ce
+        loss = loss_mae + args.lamb * loss_reg + loss_ce
 
         loss_mae_value = loss_mae.item()
         loss_reg_value = loss_reg.item()
