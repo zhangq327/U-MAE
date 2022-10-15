3dde63e6447f8b3e352251edd564019ab3357cd4
zhangq327 <81618067+zhangq327@users.noreply.github.com>
2022-10-15 21:19:38 +0800

    Update pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -7,7 +7,7 @@ python -m torch.distributed.launch --nproc_per_node=8 main_pretrain.py \
     --warmup_epochs 40 \
     --blr 1.5e-4 --weight_decay 0.05 \
     --data_path imagenet\
-    --lambda1 0.0001\
+    --lamb 0.0001\
     --reg spectral\
     --output_dir temp_dir\
     --log_dir temp_dir\
